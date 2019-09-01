a932fad57849a5031bb6527cce87c8260a00f1c4
user_full <user@localhost.localdomain>
2019-09-01 08:04:46 +0700

    Add:A check goal for AI to know a goal

--- visualisation/ascii_template.py
+++ visualisation/ascii_template.py
@@ -488,6 +488,9 @@ def make_layout(Corners, Edges, Centers):
 corners = [Corner(0, 0, 'ryb'), Corner(1, 0, 'rgy'), Corner(2, 0, 'rwg'), Corner(3, 0, 'rbw'), Corner(4, 0, 'owb'), Corner(5, 0, 'ogw'), Corner(6, 0, 'oyg'), Corner(7, 0, 'oby')]
 edges = [Edge(0, 0, 'ry'), Edge(1, 0, 'rg'), Edge(2, 0, 'rw'), Edge(3, 0, 'rb'), Edge(4, 0, 'yb'), Edge(5, 0, 'wb'), Edge(6, 0, 'wg'), Edge(7, 0, 'yg'), Edge(8, 0, 'ow'), Edge(9, 0, 'og'), Edge(10, 0, 'oy'), Edge(11, 0, 'ob')]
 centers = [Center(0, 'r'), Center(1, 'b'), Center(2, 'w'), Center(3, 'g'), Center(4, 'y'), Center(5, 'o')]
+goal_corners = [Corner(0, 0, 'ryb'), Corner(1, 0, 'rgy'), Corner(2, 0, 'rwg'), Corner(3, 0, 'rbw'), Corner(4, 0, 'owb'), Corner(5, 0, 'ogw'), Corner(6, 0, 'oyg'), Corner(7, 0, 'oby')]
+goal_edges = [Edge(0, 0, 'ry'), Edge(1, 0, 'rg'), Edge(2, 0, 'rw'), Edge(3, 0, 'rb'), Edge(4, 0, 'yb'), Edge(5, 0, 'wb'), Edge(6, 0, 'wg'), Edge(7, 0, 'yg'), Edge(8, 0, 'ow'), Edge(9, 0, 'og'), Edge(10, 0, 'oy'), Edge(11, 0, 'ob')]
+goal_centers = [Center(0, 'r'), Center(1, 'b'), Center(2, 'w'), Center(3, 'g'), Center(4, 'y'), Center(5, 'o')]
 
 # Program control
 if len(sys.argv) == 1:
@@ -502,3 +505,6 @@ else:
 corners, edges = scramble(s, corners, edges)
 layout = make_layout(corners, edges, centers)
 print_cube(layout)
+
+if (all(i.p== j.p for i,j in zip(goal_centers,centers)) and all(i.p == j.p and i.o == j.o  for i,j in zip(goal_corners,corners)) and all(i.p == j.p and i.o == j.o for i,j in zip(goal_edges,edges))):
+    print("Reach Goal")
